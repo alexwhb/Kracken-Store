ab79cd0236fcf0b121ff8d1002220f46f2f382f6
Alex Black <alex.black@blackstoneaudio.com>
2022-04-19 08:31:07 -0700

    Updates build.gradle.kts with new build targets including emulater arm64

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,7 @@ pluginManagement {
         mavenCentral()
         google()
     }
-//    resolutionStrategy {
-//        eachPlugin {
-//            if (requested.id.namespace == "com.android" || requested.id.name == "kotlin-android-extensions") {
-//                useModule("com.android.tools.build:gradle:4.1.2")
-//            }
-//        }
-//    }
+
 }
 
 
