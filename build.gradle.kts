9504e5b0abf0183d6cff0b02d9b4ad8092011fd0
Alex Black <alex.black@blackstoneaudio.com>
2022-04-19 08:31:07 -0700

    Updates build.gradle.kts with new build targets including emulater arm64

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 //import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
 plugins {
-    kotlin("multiplatform") version "1.5.30"
+    kotlin("multiplatform") version "1.6.10"
     id("maven-publish")
-    id("com.android.library")
+//    id("com.android.library")
 //    id("io.github.gradle-nexus.publish-plugin")
 //    id("signing")
 }
@@ -19,17 +19,17 @@ repositories {
 }
 
 
-android {
-    compileSdkVersion(29)
-    defaultConfig {
-        minSdkVersion(15)
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
+//android {
+//    compileSdkVersion(29)
+//    defaultConfig {
+//        minSdkVersion(15)
+//    }
+//
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_1_8
+//        targetCompatibility = JavaVersion.VERSION_1_8
+//    }
+//}
 
 
 
@@ -41,21 +41,14 @@ kotlin {
 //        publishLibraryVariants("release", "debug")
 //    }
 
-    iosArm32 {
-        binaries {
-            framework()
-        }
-    }
-    iosArm64 {
-        binaries {
-            framework()
-        }
-    }
-    iosX64 {
-        binaries {
-            framework()
-        }
-    }
+    js()
+    watchosSimulatorArm64()
+    watchos()
+
+    val iosArm32 = iosArm32()
+    val iosArm64 = iosArm64()
+    val iosX64 = iosX64()
+    val iosSimulatorArm64 = iosSimulatorArm64()
 
     sourceSets {
 
@@ -63,7 +56,7 @@ kotlin {
             dependencies {
                 implementation(kotlin("stdlib-common"))
                 api("co.touchlab:stately-common:1.1.7")
-                api("co.touchlab:stately-concurrency:1.1.7")
+                api("co.touchlab:stately-concurrency:1.2.1")
             }
             }
 
@@ -72,12 +65,6 @@ kotlin {
                 implementation(kotlin("stdlib-jdk8"))
             }
         }
-//
-//        val androidMain by getting {
-//            dependencies {
-//                implementation(kotlin("stdlib-jdk8"))
-//            }
-//        }
 
     }
 }
@@ -88,18 +75,18 @@ publishing {
     // this fetches our credentials from ~/.gradle/gradle.properties
     val mavenUser: String by project
     val mavenPassword: String by project
-
-    repositories {
-        maven {
-            setUrl("https://repos.awhb.dev/releases")
-            authentication {
-                create("basic", BasicAuthentication::class.java)
-            }
-            credentials {
-                username = mavenUser
-                password = mavenPassword
-            }
-        }
-    }
+//
+//    repositories {
+//        maven {
+//            setUrl("https://repos.awhb.dev/releases")
+//            authentication {
+//                create("basic", BasicAuthentication::class.java)
+//            }
+//            credentials {
+//                username = mavenUser
+//                password = mavenPassword
+//            }
+//        }
+//    }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,4 @@ pluginManagement {
 }
 
 
-
-
-
 rootProject.name = "Kracken"
